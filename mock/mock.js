3916ffba37101f1fbe7ce82bbc88667f9e0a9acf
Shaun Bohannon <shaun@sbohan.co.uk>
2016-03-08 17:24:22 +0000

    user correct port

--- mock/mock.js
+++ mock/mock.js
@@ -3,7 +3,7 @@
 // https://github.com/xetorthio/shmock
 
 var shmock = require('shmock'),
-	port = 8100,
+	port = process.env.PORT || 8080,
 	mock = shmock(port);
 
 mock.post('/api/session/').persist().reply(201, require('./mock_data/create-session'));
