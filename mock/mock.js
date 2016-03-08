5b137b4ba1cf4a810b20c6815b0a3ef53668c2a5
Shaun Bohannon <shaun@sbohan.co.uk>
2016-03-08 18:24:26 +0000

    Use middleware for CORS

--- mock/mock.js
+++ mock/mock.js
@@ -3,8 +3,17 @@
 // https://github.com/xetorthio/shmock
 
 var shmock = require('shmock'),
-	port = process.env.PORT || 8100,
-	mock = shmock(port);
+	port = process.env.PORT || 8100;
+
+	var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+	};
+
+var mock = shmock(port, [allowCrossDomain]);
 
 mock.post('/api/session/').persist().reply(201, require('./mock_data/create-session'));
 mock.get('/api/session/0394oiljeljkdrd/transfer/').persist().reply(200, require('./mock_data/get-transfer'));
